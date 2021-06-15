71c954246860587733ce74118c99f99900c8578a
bohdanloik <bohdanloik@gmail.com>
2021-06-15 11:47:43 +0300

    - add ReactMemo stories-

---
+++ src/components/Stories/ReactMemo.stories.tsx
@@ -0,0 +1,17 @@
+export default{
+    title: 'React.memo demo'
+}
+
+const NewMessagesCounter = (props: {count: number}) => {
+    return <div>{props.count}</div>
+}
+const User = (props: {user:Array<string>}) => {
+    return <div>{props.user.map((u, i)=> <div>{u}</div> )}</div>
+}
+
+export const Example1 = () => {
+    return <>
+        <NewMessagesCounter count={10}/>
+        <User user={['Igor', 'Marta', 'Bohdan']}/>
+    </>
+}
