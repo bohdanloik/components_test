8317941849c764a165982b1cfa8dc44fa4dcbdb6
bohdanloik <bohdanloik@gmail.com>
2021-06-16 16:44:48 +0300

    - change document.title at UseEffect stories-

--- src/components/Stories/UseEffect.stories.tsx
+++ src/components/Stories/UseEffect.stories.tsx
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default{
-    title: 'useEffect demo'
+    title: 'useEffect '
 }
 
-export const Exemple = () => {
+export const Example = () => {
     const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
+
+
+    console.log('Example');
+    
+    useEffect(()=> {
+        console.log('useEffect every render');
+        document.title = counter.toString();
+
+        
+    })
+    useEffect(()=> {
+        console.log('useEffect render only when component init');
+        document.title = counter.toString();
+
+        
+    }, [])
+    useEffect(()=> {
+        console.log('useEffect render when component init and all time when counter change');
+        document.title = counter.toString();
+
+        
+    }, [counter])
 
     return <>
-        <h1>Hello {{counter}}</h1>
-        <button  onClick={() => {setCounter(state => state +1 )}}>+</button>
+        <h1>True {counter}</h1>
+        <button  onClick={() => {setCounter(counter + 1)}}>+</button>
+        <button  onClick={() => {setFake(fake + 1)}}>+ FAKE</button>
+        <h1>Fake {fake}</h1>
         
     </>
 }
